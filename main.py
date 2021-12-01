c05fd50ed8419ae3dcc7cbbbaeaf63389b9bf18a
rkoschel <rudolf@koschel.org>
2021-12-01 21:26:26 +0100

    disable flask until it's needed

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import services.botservice as bot
 import time, json
 import re
-from threading import Thread
-from flask import Flask, request, jsonify
+#from threading import Thread
+#from flask import Flask, request, jsonify
 
 PORT = 5001
 
-app = Flask(__name__)
+#app = Flask(__name__)
 
 
 if __name__ == '__main__':
-    app.config['JSON_AS_ASCII'] = False
+    #app.config['JSON_AS_ASCII'] = False
     # runFlaskThread = Thread(target=app.run, args=('0.0.0.0', PORT, False))
     # runFlaskThread.start()
 
