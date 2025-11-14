c9436bef20e845633a7882ceb7ef58ba201441ca
rkoschel <rudolf@koschel.org>
2025-11-14 15:08:38 +0100

    use proper logging

--- config/appConfig.py
+++ config/appConfig.py
@@ -1,14 +1,31 @@
 import json
+import logging
+from logging.handlers import RotatingFileHandler
+import os
+import sys
 
-# import os
-# rootFolder = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+# logging configuration
+logging.basicConfig(  
+    level=logging.WARNING,  # Log-Level (DEBUG, INFO, WARNING, ERROR, CRITICAL)
+    format="%(asctime)s [%(levelname)s] [%(name)s] %(message)s"
+)
 
+logger = logging.getLogger("appConfig")
+if os.access("/var/log", os.W_OK):
+    rotatingFileHandler = RotatingFileHandler("/var/log/telegramNotificationBot.log", maxBytes=5*1024, backupCount=7) # Log to file with rotation
+    logger.addHandler(rotatingFileHandler)
+    logger.info("File logging enabled.")
+else:
+    logger.addHandler(logging.StreamHandler(sys.stdout)) # log to console if no file access
+    logger.warning("No write access to /var/log, file logging disabled.")
+
+# app configuration
 appConfig = None
 CONFIG_FILE_NAME    = "app.config"
 
 try:
-    configFile      = open(CONFIG_FILE_NAME, "r")
-    appConfig       = json.loads(configFile.read()) 
-    configFile.close
+    configFile = open(CONFIG_FILE_NAME, "r")
+    appConfig  = json.loads(configFile.read()) 
+    configFile.close()
 except:
-    print("could'nt load " + CONFIG_FILE_NAME)
+    logger.error("could'nt load " + CONFIG_FILE_NAME)
